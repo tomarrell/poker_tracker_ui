ae38fde4a2b0481945d0ce4c3f8d46519dd0721e
Tom Arrell <tom.arrell@gmail.com>
2017-11-18 17:54:46 +1300

    Fixed NewSession component

---
+++ src/components/Session/NewSession/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import css from './style.css';
+
+const NewSession = () => {
+  return (
+    <div className={css.newSession}>
+      Hello World
+    </div>
+  );
+};
+
+export default NewSession;
