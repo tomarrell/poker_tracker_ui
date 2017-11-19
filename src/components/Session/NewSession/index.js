e9d273bc5bf918601b4a6ada1295b9adec04d6a2
Tom Arrell <tom.arrell@gmail.com>
2017-11-20 02:41:35 +1300

    Added new session table

--- src/components/Session/NewSession/index.js
+++ src/components/Session/NewSession/index.js
@@ -1,11 +1,42 @@
 import React from 'react';
+import classnames from 'classnames';
 
 import css from './style.css';
+
+const people = [
+  { name: 'Tom' },
+  { name: 'Liam' },
+  { name: 'Hannah' },
+];
 
 const NewSession = () => {
   return (
     <div className={css.newSession}>
-      Hello World
+      <h2>New Session</h2>
+      <hr />
+      <table className={css.peopleList}>
+        <thead>
+          <tr>
+            <th>Played</th>
+            <th>Name</th>
+            <th>Buyin</th>
+            <th>Walkout</th>
+          </tr>
+        </thead>
+        {people.map(person => (
+          <tr>
+            <td className={css.checkbox}>
+              <input type="checkbox" />
+            </td>
+            <td className={css.name}>
+              {person.name}
+            </td>
+            <td className={css.buyin}>
+              $<input type="number" />
+            </td>
+          </tr>
+        ))}
+      </table>
     </div>
   );
 };
