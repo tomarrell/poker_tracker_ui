ef595be5493d2726c1107c188e2bb3a2b53f3f28
Tom Arrell <tom.arrell@gmail.com>
2017-11-20 17:04:09 +1300

    Added global button styles

--- src/components/EnterRealm/index.js
+++ src/components/EnterRealm/index.js
@@ -2,7 +2,14 @@ import React from 'react';
 
 import css from './style.css';
 
+const REALMS = [
+  'Movio',
+  'Skycity',
+  'Home Tournament',
+];
+
 const EnterRealm = () =>  {
+  const realms = REALMS;
   return (
     <div className={css.enterRealm}>
       <form>
@@ -11,9 +18,11 @@ const EnterRealm = () =>  {
         <button type="submit">Submit</button>
       </form>
 
+      <h3 className={css.recentTitle}>Recent Realms</h3>
       <ul className={css.previousRealms}>
-        <li>Something</li>
-        <li>SomethingElse</li>
+        {realms.map(realm => (
+          <li><button>{realm}</button></li>
+        ))}
       </ul>
     </div>
   );
