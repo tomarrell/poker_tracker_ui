fce70e10b593ed8ec99747646c648dc1ff467cf3
Tom Arrell <tom.arrell@gmail.com>
2017-11-21 18:29:48 +1300

    Added create user functionality, updated input styling + consistency

--- src/components/Session/NewSession/index.js
+++ src/components/Session/NewSession/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Prompt, Link } from 'react-router-dom';
 
 import css from './style.css';
@@ -19,8 +19,20 @@ const peopleList = [
 ];
 
 const DEFAULT_BUYIN = 5;
+const LEAVE_PROMPT = "Are you sure you want to leave?";
 
-const renderTable = (people) => (
+// Buyin/Walkout cols
+const buyinWalkout = [
+  <td className={css.buyin}>
+    $<input type="number" defaultValue={DEFAULT_BUYIN} step={0.10} min={0} />
+  </td>,
+  <td className={css.walkout}>
+    $<input type="number" step={0.10} />
+  </td>
+];
+
+// Table structure
+const renderTable = (people, isAddingPerson) => (
   <table className={css.peopleList}>
     <thead>
       <tr>
@@ -33,49 +45,79 @@ const renderTable = (people) => (
     <tbody>
       {people.map(person => (
         <tr key={person.id}>
-          <td className={css.played}>
-            <input type="checkbox" />
-          </td>
-          <td className={css.name}>
-            {person.name}
-          </td>
-          <td className={css.buyin}>
-            $<input type="number" defaultValue={DEFAULT_BUYIN} step={0.10} min={0} />
-          </td>
-          <td className={css.walkout}>
-            $<input type="number" step={0.10} />
-          </td>
+          <td className={css.played}><input type="checkbox" /></td>
+          <td className={css.name}>{person.name}</td>
+          {buyinWalkout}
         </tr>
       ))}
+      {isAddingPerson &&
+        <tr>
+          <td className={css.played} />
+          <td className={css.newPersonName}><input placeholder="Name..." /></td>
+          {buyinWalkout}
+        </tr>
+      }
     </tbody>
   </table>
 );
 
-const NewSession = () => {
-  return (
-    <div className={css.newSession}>
-      <Prompt 
-        when
-        message="Are you sure you want to leave?"
-      />
-      <h2>New Session</h2>
-      <hr />
-      {renderTable(peopleList)}
-      <button className={css.newPerson}>+ Add person</button>
-      <form>
-        <field>
-          <span>Session Date:* </span>
-          <input type="date" className={css.dateField} required />
-        </field>
-        <field>
-          <span>Session Time: </span>
-          <input type="time" className={css.dateField} />
-        </field>
-        <Link to="/overview" className={css.close}>Close</Link>
-        <button className={css.create} type="submit">Create Session ✔</button>
-      </form>
-    </div>
-  );
-};
+export default class NewSession extends Component {
+  constructor() {
+    super();
 
-export default NewSession;
+    this.state = {
+      isAddingPerson: false,
+    };
+
+    this.handleAddPerson = this.handleAddPerson.bind(this);
+  }
+
+  handleAddPerson() {
+    const { isAddingPerson } = this.state;
+
+    this.setState({
+      isAddingPerson: !isAddingPerson,
+    });
+  }
+
+  render() {
+    const { isAddingPerson } = this.state;
+
+    const buttonMessage = isAddingPerson ? 'x Cancel' : '+ Add person';
+
+    return (
+      <div className={css.newSession}>
+        <Prompt 
+          when
+          message={LEAVE_PROMPT}
+        />
+        <h2>New Session</h2>
+
+        <hr />
+
+        {renderTable(peopleList, isAddingPerson)}
+        <button
+          onClick={this.handleAddPerson}
+          className={css.newPerson}
+        >
+          {buttonMessage}
+        </button>
+        {isAddingPerson && <button>Save new user ✔</button>}
+
+        <form>
+          <field>
+            <span>Session Date:* </span>
+            <input type="date" className={css.dateField} required />
+          </field>
+          <field>
+            <span>Session Time: </span>
+            <input type="time" className={css.dateField} />
+          </field>
+          <Link to="/overview" className={css.close}>Close</Link>
+          <button className={css.create} type="submit">Create Session ✔</button>
+        </form>
+      </div>
+    );
+  }
+}
+
