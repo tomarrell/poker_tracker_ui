9c6d95515b6dba079e966482557440823f22c3d9
Kevin Norris <kevin.norris.90@gmail.com>
2018-02-11 11:39:48 +1300

    Add loading to leaderboard

--- src/components/Overview/reducer.js
+++ src/components/Overview/reducer.js
@@ -1,12 +1,15 @@
 import {
   FETCH_SESSIONS_SUCCESS,
   FETCH_PLAYERS_SUCCESS,
+  FETCH_REALM_INFO,
+  FETCH_REALM_INFO_SUCCESS,
 } from './actions';
 import { CREATE_PLAYER_SUCCESS, CREATE_SESSION_SUCCESS } from '../Session/actions';
 
 const initialState = {
   sessions: [],
   players: [],
+  loading: false,
 };
 
 export default (state = initialState, action) => {
@@ -34,6 +37,18 @@ export default (state = initialState, action) => {
       return {
         ...state,
         sessions: [...state.sessions, action.payload],
+      }
+    }
+    case FETCH_REALM_INFO: {
+      return {
+        ...state,
+        loading: true,
+      }
+    }
+    case FETCH_REALM_INFO_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
       }
     }
     default: {
