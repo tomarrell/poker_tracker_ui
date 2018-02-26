d68db4b75eafa57837f522bea145cf233309b377
Jacky Zhen <jacky.s.zhen@gmail.com>
2018-02-26 21:13:38 +1300

    Add loading when submitting session
    plus stop double submit

--- src/components/NewSession/index.js
+++ src/components/NewSession/index.js
@@ -4,12 +4,14 @@ import { connect } from 'react-redux';
 import { Prompt, Link } from 'react-router-dom';
 
 import Table from './Table';
+import Loading from '../Loading';
 
 import { createPlayer, createSessionRequest } from '../Session/actions';
 import { playersSelector } from '../Overview/selectors';
 
 import css from './style.css';
 import { DEFAULT_BUYIN, LEAVE_PROMPT } from './constants';
+import classnames from 'classnames'
 
 class NewSession extends Component {
   constructor(props) {
@@ -108,7 +110,9 @@ class NewSession extends Component {
   }
 
   handleCreateSession = () => {
-    const { createSessionRequest: dispatchCreateSessionRequest } = this.props;
+    const { createSessionRequest: dispatchCreateSessionRequest, loading } = this.props;
+    if (loading) return;
+
     const { currentPlayers: playerSessions, date, time } = this.state;
 
     const playDate = new Date(`${date}T${time}`);
@@ -131,6 +135,7 @@ class NewSession extends Component {
 
   render() {
     const { currentPlayers, allPlayers, isAddingPerson, newPlayerName, date, time } = this.state;
+    const { loading } = this.props;
 
     const buttonMessage = isAddingPerson ? 'x Cancel' : '+ Add Person';
 
@@ -185,10 +190,20 @@ class NewSession extends Component {
           <Link to="/overview" className={css.close}>Close</Link>
           <button
             onClick={this.handleCreateSession}
-            className={css.create}
+            className={
+              classnames(
+                {
+                  [css.create]: true,
+                  [css.loading]: loading,
+                },
+              )}
           >
             Create Session ✔
           </button>
+          <Loading
+            className={css.loadingPosition}
+            isLoading={loading}
+          />
         </div>
       </div>
     );
@@ -204,6 +219,7 @@ NewSession.propTypes = {
 export default connect(
   state => ({
     players: playersSelector(state),
+    loading: state.session.loading,
   }),
   dispatch => ({
     createPlayer: (name) => dispatch(createPlayer(name)),
--- src/components/Session/reducer.js
+++ src/components/Session/reducer.js
@@ -1,9 +1,12 @@
 import {
   FETCH_SESSION_SUCCESS,
+  CREATE_SESSION_SUCCESS,
+  CREATE_SESSION_REQUEST,
 } from './actions';
 
 const initialState = {
   currentSession: null,
+  loading: false,
 };
 
 export default (state = initialState, action) => {
@@ -12,6 +15,18 @@ export default (state = initialState, action) => {
       return {
         ...state,
         currentSession: action.payload,
+      };
+    }
+    case CREATE_SESSION_REQUEST: {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+    case CREATE_SESSION_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
       };
     }
     default: {
